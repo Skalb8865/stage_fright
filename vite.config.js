14722d13cf875a2318184471e82f699e72d45e25
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-03-22 19:01:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -68,7 +68,7 @@ export default defineConfig({
         the_NorVa: resolve(__dirname, 'html/Tours/tour_order/the_NorVa.html'),
         summit_music_hall: resolve(__dirname, 'html/Tours/tour_order/summit_music_hall.html'),
         kemba_live: resolve(__dirname, 'html/Tours/tour_order/kemba_live.html'),
-        // checkout: resolve(__dirname, 'html/Contact/checkout.html'),
+        checkout: resolve(__dirname, 'html/Contact/checkout.html'),
       },
     },
   },
