60c69998a050b09c0151630b484f43214dc7c3ad
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-03-31 16:56:22 -0400

    .

--- src/tours.js
+++ src/tours.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then((response) => response.json())
         .then((data) => {
             data.forEach((tour) => {
-                const { date, location, venue, buttonid, bg_identifier, tour_link } = tour;
+                const { date, location, venue, bg_identifier, tour_link } = tour;
                 tourCards.innerHTML += `
             <div class="tour-card">
                 <img src="${bg_identifier}" alt="">
@@ -27,39 +27,5 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
             `;
             });
-
-            // <button class="tours-btn" data-id="${buttonid}">Buy Tickets</button>
-            // Add event listeners to all buttons after they're created
-        //     document.querySelectorAll('.tours-btn').forEach(button => {
-        //         button.addEventListener('click', (e) => {
-        //             // commented out in case I use this again later
-        //             // const tourData = data.find(tour => tour.buttonid === e.target.dataset.id);
-        //             // modalTitle.textContent = "Tour Details";
-        //             // modalDate.textContent = `Date: ${tourData.date}`;
-        //             // modalLocation.textContent = `Location: ${tourData.modalLocation}`;
-        //             // modalPlace.textContent = `Place: ${tourData.place}`;
-        //             // modalPrice.textContent = `Price: ${tourData.ticketsPrice}`;
-        //             modal.showModal();
-        //         });
-        //     });
-        // })
-
-    // Close modal when clicking the close button
-    // closeBtn.addEventListener('click', () => {
-    //     modal.close();
-    // });
-
-    // // Close modal when clicking outside of it
-    // modal.addEventListener('click', (event) => {
-    //     const rect = modal.getBoundingClientRect();
-    //     const isInDialog =
-    //         event.clientX >= rect.left &&
-    //         event.clientX <= rect.right &&
-    //         event.clientY >= rect.top &&
-    //         event.clientY <= rect.bottom;
-
-    //     if (!isInDialog) {
-    //         modal.close();
-    //     }
-    });
+        });
 });
